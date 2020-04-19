a4d47b9871c52a8947b6c0a34a45b0d7dd0533ab
hpaes <herbert_pbarreto@hotmail.com>
2020-04-19 18:36:47 -0300

    challenge 4 chapter 1

---
+++ Chapter 1/challenge4.c
@@ -0,0 +1,19 @@
+// Program that prints a diamond
+#include <stdio.h>
+
+int main()
+{
+    printf("\n\t\t\t*\n");
+    printf("\n");
+    printf("\t\t*\t\t*\n");
+    printf("\n");
+    printf("\t*\t\t\t\t*\n");
+    printf("\n");
+    printf("*\t\t\t\t\t\t*\n");
+    printf("\n");
+    printf("\t*\t\t\t\t*\n");
+    printf("\n");
+    printf("\t\t*\t\t*\n");
+    printf("\n");
+    printf("\t\t\t*\n");
+}

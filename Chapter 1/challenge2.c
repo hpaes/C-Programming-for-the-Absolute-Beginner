c04af8634ce134e592ca1cf1186232060c855859
hpaes <herbert_pbarreto@hotmail.com>
2020-04-19 18:32:40 -0300

    challenge 2 chapter 1

---
+++ Chapter 1/challenge2.c
@@ -0,0 +1,7 @@
+// Program that prints my favorite quote
+#include <stdio.h>
+
+int main()
+{
+    printf("\"Failure is unimportant. It takes courage to make a fool of yourself\" - Charlie Chaplin.\n");
+}

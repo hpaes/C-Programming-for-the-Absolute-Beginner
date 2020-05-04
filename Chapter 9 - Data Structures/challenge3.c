e7cbfea053f78772d6ce264b0936fd53bdb2981f
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2020-05-04 00:41:20 -0300

    Update challenge3.c

--- Chapter 9 - Data Structures/challenge3.c
+++ Chapter 9 - Data Structures/challenge3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct phonebook
 {
-    char cName[15];
+    char cName[10];
     int iNumber;
 }
 phone;
@@ -11,34 +12,81 @@ int showMenu();
 void addNumber(phone *);
 
 int main(void)
-{
-    phone pbook[5] = {0, 0};
-    int iOpt = 0;
-
+{   
+    phone pBook[5] = {0, 0};
+    int iOpt;
+    
     do
-    {
-       iOpt = showMenu();
+    {   
+        iOpt = 0;
+        iOpt = showMenu();
 
-       switch (iOpt)
-       {
+        switch (iOpt)
+        {
             case 1:
-                for (int i = 0; i < 5; i++)
-                {
-                    if (pbook[i].iNumber != 0)
+                addNumber(pBook);
+                break;
+            
+            case 2:
+                for (int y = 0; y < 5; y++)
+                {   
+                    if (pBook[0].iNumber == 0)
                     {
-                        printf("\nName: %s\n", pbook[i].iNumber);
-                        printf("Number: %d\n", pbook[i].iNumber);
-                    
+                        printf("\nYour contact list is empty\n");
+                        break;
+                    }
+
+                    if (pBook[y].iNumber != 0)
+                    {
+                        printf("\nName: %s\n", pBook[y].cName);
+                        printf("Number: %d\n", pBook[y].iNumber);
                     }
                 }
-                break;
-       
-            case 2:
-                addNumber(pbook) ;
-                break;
-    
-       }    
-        
-    } while (iOpt != 3);
+        }   // end switch
 
+    } while (iOpt != 3);
+    
 }
+
+int showMenu()
+{
+    int answer;
+    printf("\n===========================\n");
+    printf("Welcome to the phonebook!\n");
+    printf("1\tAdd number to contact list\n2\tShow contacts from list\n3\tQuit\n");
+    printf("=============================\n");
+    printf(":> ");
+    scanf(" %d", &answer);
+    
+
+
+    return answer;
+}
+
+void addNumber(phone *list)
+{
+    
+    for (int x = 0; x < 5; x++)
+    {
+        if (list[x].iNumber != 0 && x == 4)
+        {
+            printf("\nYour contact list is full\n");
+
+            break;
+        }
+        if (list[x].iNumber == 0)
+        {   
+            char buffer[10] = {"\0"};
+            printf("\nContact %d", x + 1);
+            printf("\nName: ");
+            scanf(" %s", buffer);
+            strcpy(list[x].cName, buffer);
+            printf("\nPhone number: ");
+            scanf(" %d", &list[x].iNumber);
+
+            break;
+        }   // end if
+        
+    }   // end for
+
+}   // end addNumber
